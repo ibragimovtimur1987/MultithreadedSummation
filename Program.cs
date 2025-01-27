f9708a2cf9edb1ef40698a7854cd15a2ec4840ae
Timur <40215181+ibragimovtimur1987@users.noreply.github.com>
2025-01-27 13:25:47 +0500

    add common method

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
 ﻿using System.Diagnostics;
+int[] sizeArray = [100000, 1000000, 1000000];
 
-int[] size = [100000, 1000000, 1000000];
-
-foreach (int i in size)
+foreach (int size in sizeArray)
 {
-    Console.WriteLine($"size = {i}");
-    var array = Enumerable.Range(0, i).ToArray();
+    Console.WriteLine($"size = {size}");
+    var array = Enumerable.Range(0, size).ToArray();
     
+    SumArray(array, SequentiallySum, "SequentiallySum");
+    SumArray(array, ParallelSum, "ParallelSum");
+    SumArray(array, ThreadedSum, "ThreadedSum");
+ 
+}
+
+void SumArray(int[] array, Func<int[], long> func, string nameSum)
+{
     var stw = Stopwatch.StartNew();
-    SequentiallySum(array);
+    var sum = func(array);
     stw.Stop();
-    Console.WriteLine($"SequentiallySum time = {stw.ElapsedMilliseconds}");
-    
-    stw.Restart();
-    ParallelSum(array);
-    stw.Stop();
-    Console.WriteLine($"ParallelSum time = {stw.ElapsedMilliseconds}");
+    Console.WriteLine($"{nameSum} sum = {sum} time = {stw.ElapsedMilliseconds}");
 }
 
 long SequentiallySum(int[] array)
@@ -30,5 +32,44 @@ long SequentiallySum(int[] array)
 
 long ParallelSum(int[] array)
 {
-    return array.AsParallel().Sum(i => (long)i);
+    return array.AsParallel().Sum();
+}
+
+long ThreadedSum(int[] array)
+{
+    int numberOfThreads = 10; 
+    SumCalculator calculator = new SumCalculator();
+    Thread[] threads = new Thread[numberOfThreads];
+    int segmentSize = array.Length / numberOfThreads;
+
+    for (int numberThread = 0; numberThread < numberOfThreads; numberThread++)
+    {
+        int start = numberThread * segmentSize;
+        int end = (numberThread == numberOfThreads - 1) ? array.Length : (numberThread + 1) * segmentSize;
+        threads[numberThread] = new Thread(() => calculator.SumArraySegment(array, start, end));
+        threads[numberThread].Start();
+    }
+
+    foreach (var thread in threads)
+    {
+        thread.Join();
+    }
+
+    return calculator.Sum;
+}
+
+class SumCalculator
+{
+    public int Sum; 
+
+    public void SumArraySegment(int[] array, int start, int end)
+    {
+        int localSum = 0;
+        for (int i = start; i < end; i++)
+        {
+            localSum += array[i];
+        }
+
+        Interlocked.Add(ref Sum, localSum); 
+    }
 }
