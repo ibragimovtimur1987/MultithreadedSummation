50157570e086c655699ab67e0a88806c4fb0b462
Timur <40215181+ibragimovtimur1987@users.noreply.github.com>
2025-01-27 12:21:44 +0500

    add two methods

--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+int[] size = [100000, 1000000, 1000000];
+
+foreach (int i in size)
+{
+    Console.WriteLine($"size = {i}");
+    var array = Enumerable.Range(0, i).ToArray();
+    
+    var stw = Stopwatch.StartNew();
+    SequentiallySum(array);
+    stw.Stop();
+    Console.WriteLine($"SequentiallySum time = {stw.ElapsedMilliseconds}");
+    
+    stw.Restart();
+    ParallelSum(array);
+    stw.Stop();
+    Console.WriteLine($"ParallelSum time = {stw.ElapsedMilliseconds}");
+}
+
+long SequentiallySum(int[] array)
+{
+    int sum = 0;
+    foreach (var i in array)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+long ParallelSum(int[] array)
+{
+    return array.AsParallel().Sum(i => (long)i);
+}
